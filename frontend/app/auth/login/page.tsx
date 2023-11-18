5ee40c2c04e66a9f239bf3fe0c5265cc3d090367
yurii2007 <yurash862@gmail.com>
2023-11-18 13:58:54 +0200

    added login with google

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import AuthForm from "../authForm";
 
 const Page = () => {
   return (
-    <main>
+    <>
+      <form action="https://qa-testing-y6ws.onrender.com/api/auth/google" method="get">
+        <button type="submit">google</button>
+      </form>
+
       <AuthForm />
-    </main>
+    </>
   );
 };
 
