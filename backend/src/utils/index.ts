6a5b321e4a641dec1002f25945c23be9bd26a4d8
yurii2007 <yurash862@gmail.com>
2023-11-20 10:59:22 +0200

    bugfix
    fixed login error
    added verification email

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -3,13 +3,15 @@ import HttpError from "./HttpError";
 import ctrlWrapper from "./ctrlWrapper";
 import schemas from "./schemas";
 import getByToken from "./getUserbyToken";
+import sendEmail from "./sendMail";
 
 const utils = {
   handleMongooseError,
   HttpError,
   ctrlWrapper,
   schemas,
-  getByToken
+  getByToken,
+  sendEmail,
 };
 
 export default utils;
