748b8f444528ab3d1666c571e0f3af214909d50a
yurii2007 <yurash862@gmail.com>
2023-11-21 20:35:47 +0200

    fixed deploy failure

--- frontend/app/lib/middlewares/privateRoute.tsx
+++ frontend/app/lib/middlewares/privateRoute.tsx
@@ -1,28 +1,28 @@
-import { NextComponentType } from "next";
-import { useSelector } from "react-redux";
-import { redirect } from "next/navigation";
+// import { NextComponentType } from "next";
+// import { useSelector } from "react-redux";
+// import { redirect } from "next/navigation";
 
-import { selectIsAuth } from "@/redux/selectors";
+// import { selectIsAuth } from "@/redux/selectors";
 
-const privateRoute = (Component: NextComponentType) => {
-  const Auth = (props: any) => {
-    const isLoggedIn = useSelector(selectIsAuth);
+// const privateRoute = (Component: NextComponentType) => {
+//   const Auth = (props: any) => {
+//     const isLoggedIn = useSelector(selectIsAuth);
 
-    // redirect user to login page if he isn't logged in
-    if (!isLoggedIn) {
-      return redirect("/auth/login");
-    }
+//     // redirect user to login page if he isn't logged in
+//     if (!isLoggedIn) {
+//       return redirect("/auth/login");
+//     }
 
-    // is user is logged in, returning component
-    return <Component {...props} />;
-  };
+//     // is user is logged in, returning component
+//     return <Component {...props} />;
+//   };
 
-  // copying props from passed component
-  if (Component.getInitialProps) {
-    Auth.getInitialProps = Component.getInitialProps;
-  }
+//   // copying props from passed component
+//   if (Component.getInitialProps) {
+//     Auth.getInitialProps = Component.getInitialProps;
+//   }
 
-  return Auth;
-};
+//   return Auth;
+// };
 
-export default privateRoute;
+// export default privateRoute;
--- frontend/app/lib/middlewares/publicRoute.tsx
+++ frontend/app/lib/middlewares/publicRoute.tsx
@@ -1,28 +1,28 @@
-import { NextComponentType } from "next";
-import { useSelector } from "react-redux";
-import { redirect } from "next/navigation";
+// import { NextComponentType } from "next";
+// import { useSelector } from "react-redux";
+// import { redirect } from "next/navigation";
 
-import { selectIsLoggedIn } from "@/redux/selectors";
+// import { selectIsLoggedIn } from "@/redux/selectors";
 
-const publicRoute = (Component: NextComponentType) => {
-  const Auth = (props: any) => {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
+// const publicRoute = (Component: NextComponentType) => {
+//   const Auth = (props: any) => {
+//     const isLoggedIn = useSelector(selectIsLoggedIn);
 
-    // redirect user to home page if he is logged in
-    if (isLoggedIn) {
-      return redirect("/");
-    }
+//     // redirect user to home page if he is logged in
+//     if (isLoggedIn) {
+//       return redirect("/");
+//     }
 
-    // is user isn't logged in, returning component
-    return <Component {...props} />;
-  };
+//     // is user isn't logged in, returning component
+//     return <Component {...props} />;
+//   };
 
-  // copying props from passed component
-  if (Component.getInitialProps) {
-    Auth.getInitialProps = Component.getInitialProps;
-  }
+//   // copying props from passed component
+//   if (Component.getInitialProps) {
+//     Auth.getInitialProps = Component.getInitialProps;
+//   }
 
-  return Auth;
-};
+//   return Auth;
+// };
 
-export default publicRoute;
+// export default publicRoute;
